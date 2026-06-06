5e7dcd85b351c683845c65ba2b7800e7d02b394b
kayz <126112325+codingbykayz@users.noreply.github.com>
2026-06-06 18:24:38 -0500

    Move PostHog init back to provider, remove instrumentation-client.ts

--- app/posthog-provider.tsx
+++ app/posthog-provider.tsx
@@ -5,6 +5,17 @@ import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect, Suspense } from "react";
 
+if (typeof window !== "undefined" && !posthog.__loaded) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
+    person_profiles: "identified_only",
+    capture_pageview: false,
+    capture_pageleave: true,
+    capture_performance: true,
+  });
+}
+
 function PostHogPageview() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
